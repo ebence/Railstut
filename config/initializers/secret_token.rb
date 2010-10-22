dc7ba33640613ff3f700f1b980c88cbffa82185b
unknown <bence@.Humanex-Tech.local>
2010-10-22 16:35:00 +0200

    Initial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railstut::Application.config.secret_token = 'f28db8ddebe7c2e32301b62b7725050b1709a29ad56ed085f91a47d4f136956dd40a1f398f62373859cade4f6a2538f440c409b757b48959b360c82497227917'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Railstut::Application.config.session_store :cookie_store, :key => '_railstut_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rake db:sessions:create")
+# Railstut::Application.config.session_store :active_record_store
